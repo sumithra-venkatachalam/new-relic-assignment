43a8d6faf4bf2847837a3e597678d10693567e11
sumithra-venkatachalam <sumithrav1108@gmail.com>
2025-01-28 08:27:40 +0530

    Deleted commented lines

--- qa/cypress/e2e/header.cy.js
+++ qa/cypress/e2e/header.cy.js
@@ -1,20 +1,3 @@
-//describe("Header Navigation Links", () => {
-//   beforeEach(() => {
-//     cy.visit("/");
-//   });
-
-//   const HeaderInput = require("../fixtures/header.json");
-
-//   HeaderInput.forEach((headerCase) => {
-//     it(headerCase.testcasename, () => {
-//       if (headerCase.navigate_from)
-//         cy.get(".header__menu-item").contains(headerCase.navigate_from).click();
-//       cy.get(".header__menu-item").contains(headerCase.navigate_to).click();
-//       cy.url().should("include", headerCase.path);
-//     });
-//   });
-// });
-
 describe('Header Navigation Check', () => {
     it("Verifying Header Links", () => {
         cy.visit("/");
